f83ca820fb40ae54da20e1d7902f29963ff4a9e5
maddineni nandakishore <115626959+kishor9392@users.noreply.github.com>
2023-10-02 20:38:28 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,24 +1,27 @@
 import './index.css'
 
 const NavBar = props => {
-  const {score, topScore} = props
+  const {currentScore, isGameInProgress, topScore} = props
 
   return (
-    <div className="bg1">
-      <div className="bg2">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png "
-          alt="emoji logo"
-          className="img"
-        />
-        <h1 className="h1">Emoji Game</h1>
+    <nav className="nav-bar-container">
+      <div className="title-with-score-container">
+        <div className="logo-and-title-container">
+          <img
+            className="emoji-logo"
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+          />
+          <h1 className="title">Emoji Game</h1>
+        </div>
+        {isGameInProgress && (
+          <div className="scores-container">
+            <p className="score">Score: {currentScore}</p>
+            <p className="score">Top Score: {topScore}</p>
+          </div>
+        )}
       </div>
-
-      <div className="bg2">
-        <h1 className="h2">Score: {score}</h1>
-        <h1 className="h2">Top Score: {topScore}</h1>
-      </div>
-    </div>
+    </nav>
   )
 }
 
